6870853624b5f3cbda9512e801174a2e90b72cf0
Leticia Speda <leticiaspeda@gmail.com>
2023-01-28 19:47:00 -0300

    Feature(Extensions): criado extensao da UIView para constraints

--- App_Instagram/App_Instagram/Utils/Extensions.swift
+++ App_Instagram/App_Instagram/Utils/Extensions.swift
@@ -5,4 +5,79 @@
 //  Created by Leticia Speda on 28/01/23.
 //
 
-import Foundation
+import UIKit
+
+extension UIView {
+    func anchor(top: NSLayoutYAxisAnchor? = nil,
+                left: NSLayoutXAxisAnchor? = nil,
+                bottom:NSLayoutYAxisAnchor? = nil,
+                right:NSLayoutXAxisAnchor? = nil,
+                paddingTop: CGFloat = 0,
+                paddingLeft: CGFloat = 0,
+                paddingBottom: CGFloat = 0,
+                paddingRight: CGFloat = 0,
+                width: CGFloat? = nil,
+                height: CGFloat? = nil) {
+        
+        translatesAutoresizingMaskIntoConstraints = false
+        
+        if let top = top {
+            NSLayoutConstraint.activate([
+                topAnchor.constraint(equalTo: top, constant: paddingTop)
+            ])
+        }
+        
+        if let left = left {
+            NSLayoutConstraint.activate([
+                leftAnchor.constraint(equalTo: left, constant: paddingLeft)
+            ])
+        }
+        
+        if let bottom = bottom {
+            NSLayoutConstraint.activate([
+                bottomAnchor.constraint(equalTo: bottom, constant: paddingBottom)
+            ])
+        }
+        
+        if let right = right {
+            NSLayoutConstraint.activate([
+                rightAnchor.constraint(equalTo: right, constant: paddingRight)
+            ])
+        }
+        
+        if let width = width {
+            NSLayoutConstraint.activate([
+                widthAnchor.constraint(equalToConstant: width)
+            ])
+        }
+        
+        if let height = height {
+            NSLayoutConstraint.activate([
+                heightAnchor.constraint(equalToConstant: height)
+            ])
+        }
+    }
+    
+    func center(inView view: UIView, yConstant: CGFloat? = 0) {
+        NSLayoutConstraint.activate([
+            centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: yConstant!)
+        ])
+    }
+    
+    func centerX(inView view: UIView, yConstant: CGFloat? = 0) {
+        NSLayoutConstraint.activate([
+            centerXAnchor.constraint(equalTo: view.centerXAnchor)
+        ])
+    }
+    
+    func centerY(inView view: UIView, leftAnchor: NSLayoutXAxisAnchor? = nil, paddingLeft: CGFloat = 0, constant: CGFloat = 0) {
+        NSLayoutConstraint.activate([
+            centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ])
+        
+        if let left = leftAnchor {
+            anchor(left: left, paddingLeft: paddingLeft)
+        }
+    }
+}
