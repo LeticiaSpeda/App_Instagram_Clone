efecc9e6408128164e3ef2ce749f4ca21e68dd6a
Leticia Speda <leticiaspeda@gmail.com>
2023-01-28 10:55:22 -0300

    Refactor(SceneDelegate): retirado navigation

--- App_Instagram/Aplication/SceneDelegate.swift
+++ App_Instagram/Aplication/SceneDelegate.swift
@@ -18,9 +18,8 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let windowScene = (scene as? UIWindowScene) else { return }
         let controller = MainTabBarController()
-        let navigation = UINavigationController(rootViewController: controller)
         let safeWindow = UIWindow(windowScene: windowScene)
-        safeWindow.rootViewController = navigation
+        safeWindow.rootViewController = controller
         safeWindow.makeKeyAndVisible()
         
         window = safeWindow
