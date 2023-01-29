c0377271065744dd89bcdff099a048a89ae886be
Leticia Speda <leticiaspeda@gmail.com>
2023-01-29 13:49:08 -0300

    Refactor(FeedController): ajustado configuracao de altura e largura

--- App_Instagram/App_Instagram/Controller/FeedController.swift
+++ App_Instagram/App_Instagram/Controller/FeedController.swift
@@ -44,6 +44,11 @@ extension FeedController {
 //MARK: - UICollectionViewDelegateFlowLayout
 extension FeedController: UICollectionViewDelegateFlowLayout{
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        return CGSize(width: view.frame.width, height: 200)
+        
+        let width = view.frame.width
+        var heigh = width + 8 + 40 + 8
+        heigh += 50
+        heigh += 60
+        return CGSize(width: width, height: heigh)
     }
 }
