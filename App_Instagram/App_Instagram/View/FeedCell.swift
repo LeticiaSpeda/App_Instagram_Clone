229b126964a8dd7c93c174e0eb8203ba3dbe46e3
Leticia Speda <leticiaspeda@gmail.com>
2023-01-29 11:47:14 -0300

    Feature(FeedCell): criado imagem de perfil do feed

--- App_Instagram/App_Instagram/View/FeedCell.swift
+++ App_Instagram/App_Instagram/View/FeedCell.swift
@@ -11,13 +11,49 @@ final class FeedCell: UICollectionViewCell {
     
     //MARK: - Properties
     
+    private lazy var profileImageView: UIImageView = {
+        let image = UIImage(named: "venom-7")
+        let view = UIImageView(image: image)
+        view.contentMode = .scaleToFill
+        view.clipsToBounds = true
+        view.isUserInteractionEnabled = true
+        view.layer.cornerRadius = 40 / 2
+        view.enableCode()
+        return view
+    }()
+    
     //MARK: - LifeCycle
     override init(frame: CGRect) {
         super.init(frame: frame)
-        backgroundColor = .systemPurple
+        commoninit()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func commoninit() {
+        configureHierarchy()
+        configureConstraints()
+        configureStyle()
+    }
+    
+    private func configureHierarchy() {
+        addSubview(profileImageView)
+    }
+    
+    private func configureConstraints() {
+        profileImageView.anchor(
+            top: topAnchor,
+            left: leftAnchor,
+            paddingTop: 12,
+            paddingLeft: 12)
+        profileImageView.anchor(width: 40, height: 40)
+        
+    }
+
+    private func configureStyle() {
+        backgroundColor = .white
+        
     }
 }
