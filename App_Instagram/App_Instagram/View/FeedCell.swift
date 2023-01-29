38ffc757910a8c593030eee6bfd9c7c32ee22f63
Leticia Speda <leticiaspeda@gmail.com>
2023-01-29 13:56:41 -0300

    Feature(FeedCell): adicionado componentes: Description: - Componente de Imagem (Post) - Componente de botao de curtida - Componente de botao de comentario - Componente de botao de compartilhamento - Componente de labels [like,caption,postTime] - Adicionado stack view horizontal onde contem os componentes dos botoes de [curtida,comentario e compartilhamento]

--- App_Instagram/App_Instagram/View/FeedCell.swift
+++ App_Instagram/App_Instagram/View/FeedCell.swift
@@ -22,14 +22,76 @@ final class FeedCell: UICollectionViewCell {
         return view
     }()
     
-    
     private lazy var usernameButton: UIButton = {
         let button = UIButton(type: .system)
         button.setTitle("venom", for: .normal)
         button.setTitleColor(.black, for: .normal)
         button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 13)
-        button.addTarget(self, action: #selector(didTapUserName), for: .touchUpInside)
+        button.addTarget(
+            self, action: #selector(didTapUserName),
+            for: .touchUpInside
+        )
         return button
+    }()
+    
+    private lazy var postImageView: UIImageView = {
+        let image = UIImage(named: "venom-7")
+        let view = UIImageView(image: image)
+        view.contentMode = .scaleToFill
+        view.clipsToBounds = true
+        view.isUserInteractionEnabled = true
+        view.enableCode()
+        return view
+    }()
+    
+    private lazy var likeButton: UIButton = {
+        let button = UIButton()
+        button.setImage(UIImage(named: "like_unselected"), for: .normal)
+        button.tintColor = .black
+        return button
+    }()
+    
+    private lazy var commentButton: UIButton = {
+        let button = UIButton()
+        button.setImage(UIImage(named: "comment"), for: .normal)
+        button.tintColor = .black
+        return button
+    }()
+    
+    private lazy var shareButton: UIButton = {
+        let button = UIButton()
+        button.setImage(UIImage(named: "send2"), for: .normal)
+        button.tintColor = .black
+        return button
+    }()
+    
+    private lazy var likesLabel: UILabel = {
+        let label = UILabel()
+        label.text = "1 like"
+        label.font = UIFont.boldSystemFont(ofSize: 12)
+        return label
+    }()
+    
+    private lazy var captionLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Some test caption for now..."
+        label.font = UIFont.boldSystemFont(ofSize: 12)
+        return label
+    }()
+    
+    private lazy var postTimeLabel: UILabel = {
+        let label = UILabel()
+        label.text = "2 day ago"
+        label.font = UIFont.systemFont(ofSize: 12)
+        label.textColor = .lightGray
+        return label
+    }()
+    
+    private lazy var mainHStack: UIStackView = {
+        let stack = UIStackView(arrangedSubviews: [likeButton,commentButton,shareButton])
+        stack.axis = .horizontal
+        stack.distribution = .fillEqually
+        return stack
     }()
     
     //MARK: - LifeCycle
@@ -41,7 +103,6 @@ final class FeedCell: UICollectionViewCell {
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
     
     //MARK: - Action
     @objc func didTapUserName() {
@@ -57,6 +118,11 @@ final class FeedCell: UICollectionViewCell {
     private func configureHierarchy() {
         addSubview(profileImageView)
         addSubview(usernameButton)
+        addSubview(postImageView)
+        addSubview(likesLabel)
+        addSubview(captionLabel)
+        addSubview(postTimeLabel)
+        addSubview(mainHStack)
     }
     
     private func configureConstraints() {
@@ -70,10 +136,16 @@ final class FeedCell: UICollectionViewCell {
                                leftAnchor: profileImageView.rightAnchor,
                                paddingLeft: 8)
         
+        postImageView.anchor(top: profileImageView.bottomAnchor, left: leftAnchor, right: rightAnchor, paddingTop: 8)
+        postImageView.heightAnchor.constraint(
+            equalTo: widthAnchor,
+            multiplier: 1
+        ).isActive = true
+        
+        mainHStack.anchor(top: postImageView.bottomAnchor, width: 120, height: 50)
     }
-
+    
     private func configureStyle() {
         backgroundColor = .white
-        
     }
 }
