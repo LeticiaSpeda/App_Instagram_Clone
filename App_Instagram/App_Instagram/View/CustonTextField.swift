553768ca0d0f8f11c7a901d8996ea186ab4478cc
Leticia Speda <leticiaspeda@gmail.com>
2023-02-09 11:00:14 -0300

    Feature(CustonTextField): criado textField customizado

--- App_Instagram/App_Instagram/View/CustonTextField.swift
+++ App_Instagram/App_Instagram/View/CustonTextField.swift
@@ -5,4 +5,31 @@
 //  Created by Leticia Speda on 09/02/23.
 //
 
-import Foundation
+import UIKit
+
+final class CustonTextField: UITextField {
+    
+    init(placeholder: String) {
+        super.init(frame: .zero)
+        
+        let spacer = UIView()
+        spacer.setDimensions(height: 50, width: 12)
+        leftView = spacer
+        leftViewMode = .always
+        
+        borderStyle = .none
+        textColor = .white
+        keyboardAppearance = .dark
+        keyboardType = .emailAddress
+        backgroundColor = UIColor(white: 1, alpha: 0.1)
+        setHeight(50)
+        layer.cornerRadius = 8
+        attributedPlaceholder = NSAttributedString(string: placeholder, attributes: [.foregroundColor: UIColor(white: 1, alpha: 0.7)])
+        isSecureTextEntry = true
+    }
+    
+    required init?(coder: NSCoder) { nil }
+    
+    
+    
+}
