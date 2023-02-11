77ff2f72d4fa63181f2eda34cb8e3008cd0b3811
Leticia Speda <leticiaspeda@gmail.com>
2023-02-11 20:59:31 -0300

    Feature(UIViewController+Extensions): criado extensao para cor gradient da tela

---
+++ App_Instagram/App_Instagram/Utils/UIViewController+Extensions.swift
@@ -0,0 +1,19 @@
+//
+//  UIViewController+Extensions.swift
+//  App_Instagram
+//
+//  Created by Leticia Speda on 09/02/23.
+//
+
+import UIKit
+
+extension UIViewController {
+    func configureGradienteLayer() {
+        let gradient = CAGradientLayer()
+        gradient.colors = [UIColor.systemPurple.cgColor, UIColor.systemPink.cgColor, UIColor.systemOrange.cgColor, UIColor.systemYellow.cgColor]
+        gradient.locations = [0,1]
+        view.layer.addSublayer(gradient)
+        gradient.frame = view.frame
+    }
+}
+
