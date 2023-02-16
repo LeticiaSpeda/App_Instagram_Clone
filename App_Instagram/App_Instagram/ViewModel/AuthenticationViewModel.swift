e90455507dfe35972e8607285e0542c4be65fa0c
Leticia Speda <leticiaspeda@gmail.com>
2023-02-16 11:24:04 -0300

    Feature(AuthenticationViewModel): adicionado configuracao para quando o usuario digitar nos campos de textos o botao fique inativo ate que complete todos os campos e o mesmo possa ser ativado

--- App_Instagram/App_Instagram/ViewModel/AuthenticationViewModel.swift
+++ App_Instagram/App_Instagram/ViewModel/AuthenticationViewModel.swift
@@ -18,7 +18,11 @@ struct LoginViewModel {
     
     
     var buttomBackgroundColor: UIColor {
-        return formIsValid ? #colorLiteral(red: 0.2196078449, green: 0.007843137719, blue: 0.8549019694, alpha: 1) : #colorLiteral(red: 0.5568627715, green: 0.3529411852, blue: 0.9686274529, alpha: 1).withAlphaComponent(0.5)
+        return formIsValid ? #colorLiteral(red: 0.3647058904, green: 0.06666667014, blue: 0.9686274529, alpha: 1).withAlphaComponent(0.5) : #colorLiteral(red: 0.5568627715, green: 0.3529411852, blue: 0.9686274529, alpha: 1).withAlphaComponent(0.5)
+    }
+    
+    var buttonTitleColor: UIColor {
+        return formIsValid ? .white : UIColor(white: 1, alpha: 0.67)
     }
 }
  
