e98f5b44b67488f9b37b78d5eddcc4e5ac4c2068
Leticia Speda <leticiaspeda@gmail.com>
2023-03-06 16:51:16 -0300

    Feature(ImageUploader): adicionado configuracoes para salvar a imagem no dados do firebase

--- App_Instagram/App_Instagram/API/ImageUploader.swift
+++ App_Instagram/App_Instagram/API/ImageUploader.swift
@@ -5,4 +5,23 @@
 //  Created by Leticia Speda on 03/03/23.
 //
 
-import Foundation
+import FirebaseStorage
+
+struct ImageUploader {
+    static func uploadImage(image: UIImage, completion: @escaping(String)-> Void) {
+        guard let imageData = image.jpegData(compressionQuality: 0.75) else { return }
+        let filename = NSUUID().uuidString
+        let ref = Storage.storage().reference(withPath: "/profile_images/\(filename)")
+        
+        ref.putData(imageData, metadata: nil) { metadata, error in
+            if let error = error {
+                print("DEBUG: Failed to upload image \(error.localizedDescription)")
+                return
+            }
+            ref.downloadURL { url, error in
+                guard let imageUrl = url?.absoluteString else { return }
+                completion(imageUrl)
+            }
+        }
+    }
+}
